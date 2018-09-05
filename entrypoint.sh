da805bcbabfda307fec4e477dc4888f9ce390e64
Freddy Wieffering <fwieffering@gmail.com>
2018-09-05 10:56:14 -0500

    add region configuration, log level

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+export AWS_REGION="us-east-1"
 username=$(ssm_get_parameter /techops/cloud-engineering/bdp/dev/chartmuseum_user)
 password=$(ssm_get_parameter /techops/cloud-engineering/bdp/dev/chartmuseum_pass)
 # may as well hard code the repo url
 
 # invoke plugin
-/bin/drone-chartmuseum --repo-url "https://chartmuseum.spsapps.net/" --username $username --password $password
+/bin/drone-chartmuseum --repo-url "https://chartmuseum.spsapps.net/" --username $username --password $password --log-level debug
