74eb12569154f9e9dc5b32dd49e44a471896f048
Freddy Wieffering <fwieffering@gmail.com>
2018-09-05 09:32:04 -0500

    Add sps special sauce

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+username=$(ssm_get_parameter /techops/cloud-engineering/bdp/dev/chartmuseum_user)
+password=$(ssm_get_parameter /techops/cloud-engineering/bdp/dev/chartmuseum_pass)
+# may as well hard code the repo url
+
+# invoke plugin
+/bin/drone-chartmuseum --repo-url "https://chartmuseum.spsapps.net/" --username $username --password $password
